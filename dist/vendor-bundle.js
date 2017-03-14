b9b1a6060da44928e3f04d6960ed7ee55fbc30c6
David Taylor <dtaylor113@gmail.com>
2017-03-14 09:02:28 -0400

    Removing config.local.js from repo.

--- dist/vendor-bundle.js
+++ dist/vendor-bundle.js
@@ -136,7 +136,7 @@
 /******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
 /******/
 /******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "https://localhost:9000/";
+/******/ 	__webpack_require__.p = "http://localhost:9000/";
 /******/
 /******/ 	// on error function for async loading
 /******/ 	__webpack_require__.oe = function(err) { console.error(err); throw err; };
