6cd3699f9e34561f718271f5d2d941fa451ca0f4
Jeffrey Phillips <jephilli@redhat.com>
2017-05-17 15:28:27 -0400

    Show one row of SasS offerings, add Show More/Less

--- src/components/saas-list/saas-list.controller.ts
+++ src/components/saas-list/saas-list.controller.ts
@@ -2,15 +2,59 @@ import * as angular from 'angular';
 import * as _ from 'lodash';
 
 export class SaasListController implements angular.IController {
-  public ctrl: any = this;
+  static $inject = ['$scope', '$window', '$element'];
 
-  public hasSaasOfferings (): boolean {
+  public ctrl: any = this;
+  private $scope: any;
+  private $window: any;
+  private $element: any;
+  private debounceResize: any;
+
+  constructor($scope: any, $window: any, $element: any) {
+    this.$scope = $scope;
+    this.$window = $window;
+    this.$element = $element;
+    this.ctrl.sassListExpanded = false;
+    this.ctrl.itemsOverflow = false;
+  }
+
+  public $postLink() {
+    this.debounceResize = _.debounce(this.onWindowResize, 50, { maxWait: 250 });
+    angular.element(this.$window).on('resize', this.debounceResize);
+
+    this.updateListExpandVisibility();
+  }
+
+  public $onDestroy() {
+    angular.element(this.$window).off('resize', this.debounceResize);
+  }
+
+  public hasSaasOfferings(): boolean {
     return !_.isEmpty(this.ctrl.saasOfferings);
   }
 
   public $onChanges(onChangesObj: angular.IOnChangesObject) {
     if ((onChangesObj.saasOfferings && !onChangesObj.saasOfferings.isFirstChange()) ) {
       this.ctrl.saasOfferings = onChangesObj.saasOfferings.currentValue;
+      this.updateListExpandVisibility();
     }
   }
+
+  public toggleListExpand() {
+    this.ctrl.sassListExpanded = !this.ctrl.sassListExpanded;
+  }
+
+  private updateListExpandVisibility() {
+    var windowWidth: number = this.$window.innerWidth;
+    var offeringCount: number = _.size(this.ctrl.saasOfferings);
+
+    this.ctrl.itemsOverflow = (windowWidth >= this.$window.patternfly.pfBreakpoints.tablet) &&
+      ((offeringCount > 4) || (windowWidth < this.$window.patternfly.pfBreakpoints.desktop && offeringCount > 2));
+  }
+
+  private onWindowResize = () => {
+    this.$scope.$evalAsync(() => {
+      this.updateListExpandVisibility();
+    });
+  };
 }
