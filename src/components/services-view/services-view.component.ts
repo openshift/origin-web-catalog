c0f88db3ab96ba186c0d24cf9b87c2ce079d9260
Robb Hamilton <rhamilto@redhat.com>
2017-07-14 13:58:41 -0400

    Adding Deploy Image and Import YAML / JSON options

--- src/components/services-view/services-view.component.ts
+++ src/components/services-view/services-view.component.ts
@@ -4,6 +4,8 @@ export const servicesView: angular.IComponentOptions = {
   bindings: {
     baseProjectUrl: '@',
     catalogItems: '<',
+    onDeployImageSelected: '<',
+    onFromFileSelected: '<'
   },
   controller: ServicesViewController,
   template: require('./services-view.html')
