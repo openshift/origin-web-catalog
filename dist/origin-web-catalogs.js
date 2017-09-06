6253e8fde3cd0a5a89ba6c3ea00422b94cc9c060
Samuel Padgett <spadgett@redhat.com>
2017-09-06 08:48:00 -0400

    Update Middleware categories

--- dist/origin-web-catalogs.js
+++ dist/origin-web-catalogs.js
@@ -293,25 +293,50 @@ webpackJsonp([ 0, 1 ], [ function(e, t) {
             id: "middleware",
             label: "Middleware",
             subCategories: [ {
-                id: "jboss",
-                label: "JBoss",
-                tags: [ "jboss" ],
-                icon: "font-icon icon-openjdk"
-            }, {
-                id: "fuse",
-                label: "Fuse",
-                tags: [ "fuse" ],
-                icon: "font-icon icon-openjdk"
-            }, {
                 id: "amq",
                 label: "A-MQ",
                 tags: [ "amq" ],
-                icon: "font-icon icon-openjdk"
+                icon: "font-icon icon-jboss"
             }, {
-                id: "bpm",
-                label: "BPM",
-                tags: [ "bpm" ],
-                icon: "font-icon icon-openjdk"
+                id: "processserver",
+                label: "BPM Suite",
+                tags: [ "processserver" ],
+                icon: "font-icon icon-jboss"
+            }, {
+                id: "decisionserver",
+                label: "BRMS",
+                tags: [ "decisionserver" ],
+                icon: "font-icon icon-jboss"
+            }, {
+                id: "datagrid",
+                label: "Data Grid",
+                tags: [ "datagrid" ],
+                icon: "font-icon icon-jboss"
+            }, {
+                id: "datavirt",
+                label: "Data Virt",
+                tags: [ "datavirt" ],
+                icon: "font-icon icon-jboss"
+            }, {
+                id: "eap",
+                label: "EAP",
+                tags: [ "eap" ],
+                icon: "font-icon icon-jboss"
+            }, {
+                id: "fuse",
+                label: "Fuse",
+                tags: [ "fuse", "jboss-fuse" ],
+                icon: "font-icon icon-jboss"
+            }, {
+                id: "tomcat",
+                label: "Tomcat",
+                tags: [ "tomcat" ],
+                icon: "font-icon icon-jboss"
+            }, {
+                id: "sso",
+                label: "SSO",
+                tags: [ "sso" ],
+                icon: "font-icon icon-jboss"
             } ]
         }, {
             id: "cicd",
