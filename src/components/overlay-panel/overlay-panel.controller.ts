7a7bfbfd25ad23b1458e747d00fb3cdba45d81d9
Jeffrey Phillips <jephilli@redhat.com>
2018-05-08 08:18:58 -0400

    Keep focus inside overlay panel when displayed
    Set initial focus appropriately in wizards
    Requires upgrade to Angular-Patternfly 4.18

--- src/components/overlay-panel/overlay-panel.controller.ts
+++ src/components/overlay-panel/overlay-panel.controller.ts
@@ -1,16 +1,24 @@
 import * as angular from 'angular';
 import * as $ from 'jquery';
+import * as _ from 'lodash';
 
 export class OverlayPanelController implements angular.IController {
-  static $inject = ['$document', '$scope'];
+  static $inject = ['$document', '$scope', '$element'];
 
   public ctrl: any = this;
   private $document: any;
   private $scope: any;
+  private $element: any;
+  private focusableElementList: any;
+  private tababbleSelector: String = 'a[href], area[href], input:not([disabled]), ' +
+    'button:not([disabled]),select:not([disabled]), textarea:not([disabled]), ' +
+    'iframe, object, embed, *[tabindex], *[contenteditable=true]';
 
-  constructor ($document: any, $scope: any) {
+
+  constructor ($document: any, $scope: any, $element: any) {
     this.$document = $document;
     this.$scope = $scope;
+    this.$element = $element;
     this.ctrl.shown = false;
   }
 
@@ -43,16 +51,16 @@ export class OverlayPanelController implements angular.IController {
   private showDialog = () => {
     this.ctrl.shown = true;
     $('body').addClass('overlay-open');
-    this.$document.on('keydown', this.closeOnEsc);
+    this.$document.on('keydown', this.keydownHandler);
   };
 
   private hideDialog = () => {
     this.ctrl.shown = false;
     $('body').removeClass('overlay-open');
-    this.$document.off('keydown', this.closeOnEsc);
+    this.$document.off('keydown', this.keydownHandler);
   };
 
-  private closeOnEsc = (event: any) => {
+  private keydownHandler = (event: any) => {
     if (event.which === 27) {
       // Only close this overlay if another handler has not called 
       // preventDefault() on this event. This means only one overlay/modal will
@@ -63,6 +71,60 @@ export class OverlayPanelController implements angular.IController {
           this.closePanel();
         });
       }
+    } else if (event.which === 9) {
+      // Handle the tab key, keep the focus within the elements of the overlay panel
+      this.loadFocusElementList();
+      var focusChanged = false;
+      if (event.shiftKey) {
+        if (this.isFocusInFirstItem(event)) {
+          focusChanged = this.focusLastFocusableElement();
+        }
+      } else {
+        if (this.isFocusInLastItem(event)) {
+          focusChanged = this.focusFirstFocusableElement();
+        }
+      }
+
+      if (focusChanged) {
+        event.preventDefault();
+        event.stopPropagation();
+      }
+    }
+  };
+
+  private focusFirstFocusableElement = () => {
+    if (this.focusableElementList.length > 0) {
+      this.focusableElementList[0].focus();
+      return true;
+    }
+    return false;
+  };
+
+  private focusLastFocusableElement = () => {
+    if (this.focusableElementList.length > 0) {
+      this.focusableElementList[this.focusableElementList.length - 1].focus();
+      return true;
+    }
+    return false;
+  };
+
+  private isFocusInFirstItem = (evt: any) => {
+    if (this.focusableElementList.length > 0) {
+      return (evt.target || evt.srcElement) === this.focusableElementList[0];
+    }
+    return false;
+  };
+
+  private isFocusInLastItem = (evt: any) => {
+    if (this.focusableElementList.length > 0) {
+      return (evt.target || evt.srcElement) === this.focusableElementList[this.focusableElementList.length - 1];
+    }
+    return false;
+  };
+
+  private loadFocusElementList = () => {
+    if (_.size(this.$element)) {
+      this.focusableElementList = this.$element[0].querySelectorAll(this.tababbleSelector);
     }
   };
 }
